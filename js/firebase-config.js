3b1b2ee63d29eba7fae9c2accedb2d0241d16fb9
DhimasPrasojo <dhimasprasojo17@gmail.com>
2024-10-23 10:40:24 +0800

    Tes Fitur Push Notification

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,3 +10,41 @@ const firebaseConfig = {
 };
 
 const app = firebase.initializeApp(firebaseConfig);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.log("Service Worker registration failed:", error);
+    });
+}
+
+// Initialize Firebase
+const messaging = firebase.messaging();
+
+// Optional: request permission for notifications
+messaging
+  .requestPermission()
+  .then(() => messaging.getToken())
+  .then((token) => {
+    console.log("FCM Token:", token);
+  })
+  .catch((error) => {
+    console.error("Error getting FCM token", error);
+  });
+
+// firebase-messaging-sw.js
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon.png",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
